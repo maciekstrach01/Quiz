267ce4fdd43cf768db3ce8636af88f3ed6b3a575
Maciej Strach <maciej.strach01@gmail.com>
2023-12-01 12:31:47 +0100

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,121 @@
+const questions = [
+    {
+              number:1,        
+              question:'Assuming we wanted to eat once a day in another New York restaurant, how many years would it take to visit every restaurant in the New York area?',
+              answer:'Around 75',
+              options:[
+           'Around 10',
+           'Around 25',
+           'Around 75', 
+           'Around 100'
+           ]
+     }, 
+     {
+             number:2,
+             question: 'What is photosynthesis?',
+             answer: 'The process of producing chemical energy',
+             options: [
+           'The process of gas exchange in the lungs',
+           'The process of digestion in herbivorous organisms',
+           'The process of excretion of toxic substances from the body',
+           'The process of producing chemical energy'
+           ]
+     }, 
+     {
+             number:3,
+             question: 'In philosophy and psychology, a concept intended to express the view that all knowledge comes only from experience',
+             answer: 'Tabula rasa',
+             options: [
+           'Biological determinism',
+           'Innatism',
+           'Tabula rasa',
+           'Educationism'
+           ]
+     }, 
+     {
+             number:4,
+             question:'What is the name of the line separating Poland from its eastern neighbours?',
+             answer:'Curzon Line',
+             options:[
+                     'The Maginot Line',
+                     'Curzon Line',
+                     'Oder-Nysa line', 
+                     'The Molotov Line'
+                     ]
+     }, 
+     {
+             number:5,
+             question:'Who do you think owns the title of the most profitable music album in history?',
+             answer:'Michael Jackson-"Thriller"',
+             options:[
+           'AC/DC-"Back in Black"',
+           'Whitney Houston-"The Bodyguard"',
+           'Michael Jackson-"Thriller"',
+           'Adele-"21"'
+           ]
+     }, 
+     {
+            number:6,
+            question:'There is no scientific evidence to support this, but in his laboratory, German scientist Dan Frost proved that diamonds can be made from:',
+            answer:'peanut butter',
+            options:[
+            'peanut butter',
+            'cheese',
+            'potatoes',
+            'pasta'
+        ]
+      }, 
+  
+      {
+        number:7,
+        question:'In the legal context, what does the term "precedent" refer to?',
+        answer:'A legal decision that guides future similar cases',
+        options:[
+            'A legal term for a defendant in a court case',
+            'A legal doctrine that states all laws are subject to change',
+            'A legal decision that guides future similar cases',
+            'A legal term for a plaintiff in a court case'
+    ]
+  }, 
+  
+  {
+        number:8,
+        question:'Which car brand was the most popular among buyers in 2022?',
+        answer:'Toyota',
+        options:[
+        'Ford',
+        'Toyota',
+        'Tesla',
+        'BMW'
+  ]
+  }, 
+  
+  
+  {
+    number:9,
+    question:'Which of these animals is immortal?',
+    answer:'Hydra',
+    options:[
+    'Polar shark',
+    'The sponge Cinachyra antarctica',
+    'Icelandic Cyprinus',
+    'Hydra'
+  ]
+  }, 
+  
+  
+  {
+    number:10,
+    question:'What Pneumonoultra- \n -microscopicsilicovolcanoconiosi means, \n officially the longest word in the world ?',
+    answer:'lung disease',
+    options:[
+    'kind of fish',
+    'epilepsy',
+    'lung disease',
+    'ancient spell'
+  ]
+  }, 
+  
+  
+      ]
+  
